f72558c08200d96aba952ad15e7d93ebf3cce8c6
Juanjo <juanjo@juanjofp.com>
2014-12-29 17:37:27 +0100

    TEst superado

--- test/index.js
+++ test/index.js
@@ -2,13 +2,14 @@ var expect = require('chai').expect,
     weather = require('../index').weather;
 
 describe('Module Weather', function(){
-  var fake = {
-    position: {lat: 38.032024, lng: -1.124372},
-    temperature: {max: 22, min: 8, current: 12, pressure: 1013, humidity: 89},
-    wind: {speed: 9, deg: 187.020}
-  };
 
-  it('get coords from param', function(){
-    expect(weather(38.032024, -1.124372)).to.deep.equal(fake);
+  it('get info from coords', function(){
+    weather(38.032024, -1.124372, function(data){
+      expect(data).to.be.an('object');
+      expect(data).to.include.keys('name');
+      expect(data).to.include.keys('position');
+      expect(data).to.include.keys('temp');
+      expect(data).to.include.keys('wind');
+    });
   });
 });
